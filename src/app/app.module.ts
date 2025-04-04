2a0a5a6bdfe047dc9f87e122e02be06112281305
anaghamanoj09 <anaghamanojoo9@gmail.com>
2025-04-04 07:49:35 +0530

    sample commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardsComponent } from './cards/cards.component';
+import { Cards2Component } from './cards2/cards2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CardsComponent,
+    Cards2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
